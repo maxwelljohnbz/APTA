792d1db243f878ca6810b0eca6f1876a8864d1e5
Maxwell John Bottone-Zanders <118388379+maxwelljohnbz@users.noreply.github.com>
2025-06-18 15:50:57 -0400

    App Creation
    Made an XCODE project for apta

---
+++ APTA/APTA/APTAApp.swift
@@ -0,0 +1,17 @@
+//
+//  APTAApp.swift
+//  APTA
+//
+//  Created by Maxwell  on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct APTAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APTA/APTATests/APTATests.swift
@@ -0,0 +1,16 @@
+//
+//  APTATests.swift
+//  APTATests
+//
+//  Created by Maxwell  on 6/18/25.
+//
+
+import Testing
+
+struct APTATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
